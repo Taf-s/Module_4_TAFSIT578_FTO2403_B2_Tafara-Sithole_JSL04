902a34727dd83dccaca9dac790b353083b49b937
taf-s <tafara.sithole@gmail.com>
2024-05-16 11:46:17 +0200

    updated each challenge with a ternary operator

--- index.js
+++ index.js
@@ -4,15 +4,15 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+const userPreference = 'regular';
+console.log(userPreference === 'sugar-free' ? "Sugar-free" : "Regular") 
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+console.log(cansLeft < 5 ? "Time to restock" : "We're stocked");
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,16 +20,14 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
-
+console.log(heartRate < 100 ? "Boost Needed" : "Energy levels are high");
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
-
+console.log(currentTemp <= 5 ? "Chilled to Perfection" : "Needs a Cooler!");
 
 // #### Challenge 5: Late Night Coding Session
 // Determine if it's a good idea to have a Monster Energy drink based on the current hour.
@@ -37,15 +35,13 @@ console.log(``);
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
+
 let currentHour = 22; 
+console.log(currentHour <= 7 && currentHour < 24 ? "Unleash the Beast" : "Better stick to water");
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
-
-
-
-
 
 
 // In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
